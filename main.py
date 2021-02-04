29f40d127d740095d08847789739d5a66bc71797
zfdupont <zachary.dupont311@gmail.com>
2021-02-04 18:51:32 -0500

    first real commit

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from yahoo_fin import stock_info as si
+from time import time
+from datetime import datetime
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# AAPL AMZN TSLA gme AMC SPY MSFT SBUX FB
+
+tags = input("").split(" ")
+
+t = time()
+while 1:
+    # print(time() - t)
+    if (time() - t) > 5:
+        for tick in tags:
+            print(f"{tick.upper()}\t\t{si.get_live_price(tick)}")
+        t = time()
+        print(datetime.now().strftime("%H:%M:%S"))
 
