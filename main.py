e44788b458fd12f2f40f5a40abd6fec77eb02e75
zfdupont <zachary.dupont311@gmail.com>
2021-02-04 20:35:49 -0500

    added change since last check and constant variable for time interval between scrapes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from yahoo_fin import stock_info as si
 from time import time
 from datetime import datetime
+from decimal import Decimal
 
 import os;
 
@@ -15,18 +16,42 @@ class colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# TEST TICKETS
 # AAPL AMZN TSLA gme AMC SPY MSFT SBUX FB
 
 tags = input("").split(" ")
 
 t = time()
 clear = lambda: os.system('cls')
-while 1:
+
+prices = []
+
+def print_watching():
+    clear()
+    temp = []
+    global prices
+    for i in range(len(tags)):
+        tick = tags[i]
+        x = si.get_live_price(tick)
+        print(f"{tick.upper()}\t\t{x}", end="")
+        if prices:
+            change = "="
+            if(x > prices[i]): change = "▲"
+            if(x < prices[i]): change = "▼"
+            print(f"({change}{round(abs(x - prices[i]), 2)})")
+        else: print()
+        temp.append(round(x, 2))
+    prices = temp[:]
+    print(datetime.now().strftime("%H:%M:%S"))
+
+print_watching()
+
+
+CONST_INTERVAL = 5
+
+while True:
     # print(time() - t)
-    if (time() - t) > 5:
-        clear()
-        for tick in tags:
-            print(f"{tick.upper()}\t\t{si.get_live_price(tick)}")
+    if (time() - t) > CONST_INTERVAL:
+        print_watching()
         t = time()
-        print(datetime.now().strftime("%H:%M:%S"))
 
